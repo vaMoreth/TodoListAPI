14470f673d190b527cb5b82a27b16aba83fb6481
vaMoreth <jummyvasya24@gmail.com>
2024-12-04 13:02:10 +0100

    Add Post endpoint in controller

--- todo-list-api/src/todo/todo.controller.ts
+++ todo-list-api/src/todo/todo.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TodoService } from './todo.service';
+import { CreateTodoDto } from './create-todo.dto';
 
 @Controller('todo')
 export class TodoController {
@@ -8,5 +9,11 @@ export class TodoController {
   @Get()
   getAllTodos() {
     return this.todoService.getTodos();
+  }
+
+  @Post()
+  createTodo(@Body() createTodoDto: CreateTodoDto) {
+    const { title, description } = createTodoDto;
+    return this.todoService.createTodo(title, description);
   }
 }
--- todo-list-api/src/todo/todo.service.ts
+++ todo-list-api/src/todo/todo.service.ts
@@ -3,8 +3,19 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TodoService {
   private todos = [];
+  private id = 1;
 
   getTodos() {
     return this.todos;
+  }
+
+  createTodo(title: string, description?: string) {
+    const newTodo = {
+      id: this.id++,
+      title,
+      description,
+    };
+    this.todos.push(newTodo);
+    return newTodo;
   }
 }
